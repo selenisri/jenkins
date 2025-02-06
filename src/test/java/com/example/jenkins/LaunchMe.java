7441c54d06609802872d4b93fdb7aaee7d7e48e3
selenisri <91556917+selenisri@users.noreply.github.com>
2025-02-06 23:50:31 +0530

    Update the changes

---
+++ src/test/java/com/example/jenkins/LaunchMe.java
@@ -0,0 +1,33 @@
+package com.example.jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchMe {
+	
+	
+        @Test
+	    public void launchBrowser() {
+	        // Setup WebDriver Manager for Chrome
+	        WebDriverManager.chromedriver().setup();
+	        
+	        // Instantiate Chrome WebDriver
+	        WebDriver driver = new ChromeDriver();
+	        
+	        // Open a website
+	        driver.get("https://www.google.com");
+	        
+	        // Maximize the window
+	        driver.manage().window().maximize();
+	        
+	        // Perform other actions (if needed)
+
+	        // Close the browser
+	        driver.quit();
+	    }
+	}
+
+
