72676c696fd8293a201310a7ae76e9c10fd87399
selenisri <91556917+selenisri@users.noreply.github.com>
2025-02-07 10:30:53 +0530

    Added files to Remote Repository with POM and Launch me file

--- src/test/java/com/example/jenkins/LaunchMe.java
+++ src/test/java/com/example/jenkins/LaunchMe.java
@@ -11,22 +11,22 @@ public class LaunchMe {
 	
         @Test
 	    public void launchBrowser() {
-//	        // Setup WebDriver Manager for Chrome
-//	        WebDriverManager.chromedriver().setup();
-//	        
-//	        // Instantiate Chrome WebDriver
-//	        WebDriver driver = new ChromeDriver();
-//	        
-//	        // Open a website
-//	        driver.get("https://www.google.com");
-//	        
-//	        // Maximize the window
-//	        driver.manage().window().maximize();
-//	        
-//	        // Perform other actions (if needed)
-//
-//	        // Close the browser
-//	        driver.quit();
+	        // Setup WebDriver Manager for Chrome
+	        WebDriverManager.chromedriver().setup();
+	        
+	        // Instantiate Chrome WebDriver
+	        WebDriver driver = new ChromeDriver();
+	        
+	        // Open a website
+	        driver.get("https://www.google.com");
+	        
+	        // Maximize the window
+	        driver.manage().window().maximize();
+	        
+	        // Perform other actions (if needed)
+
+	        // Close the browser
+	        driver.quit();
         	
         	System.out.println("This is Sriram");
 	    }
